07bdd16939e6b451e16697333973adefbbf71f43
$teirner <90169845+Steirner@users.noreply.github.com>
2025-08-15 10:49:42 +0800

    Add files via upload

---
+++ Student4.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+<?php
+echo "Full Name: Mart Keinth Balero ";
+echo "<br>";
+
+echo "Age: 18";
+echo "<br>";
+
+echo "Gender: Male";
+echo "<br>";
+
+echo "Birthday: October 5 2006";
+echo "<br>";
+
+echo "Address: Sf2 Limay, Bataan";
+echo "<br>";
+?>
+</br>
+    <a href="index.php">Back to index</a>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+      <title>Student Directory</title>
+      <style>
+      
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f9;
+    text-align: center;
+    color: #333;
+    margin: 0;
+    padding: 0;
+}
+
+h1 {
+    text-align: center;
+    color: #2c3e50;
+    margin-bottom: 30px;
+}
+
+a{
+    color: #2c3e50; 
+}
+
+</style>
+  </head>
+  
+  <body>
+    <br>
+      <h1>Student Directory</h1>
+      <br>
+          <a href="Student1.php">About to Renz Paulo Deguzman</a>
+</br>
+<br>
+          <a href="Student2.php">About to Vaynard Vrenz Abueg</a>
+        </br>
+        <br>
+          <a href="Student3.php">About to Aaron Lee Herjas</a>
+</br>
+<br>
+          <a href="Student4.php">About to Mart Keinth Balero</a>
+</br>
+<br>
+          <a href="Student5.php">About to Jhon lloyd Ajero</a>
+</br>
+<br>
+          <a href="Student6.php">About to Rodrianne Bantog</a>
+</br>
+<br>
+          <a href="Student7.php">About to John Christian hernandez</a>
+</br>
+<br>
+          <a href="Student8.php"> About to Jomarie Alberto</a>
+</br>
+<br>
+          <a href="Student9.php"> About to Romelyn Joy Dominico</a>
+</br>
+<br>
+          <a href="Student10.php">About to Vladymier balangat </a>
+</br>
+<br>
+          <a href="Student11.php">About to Peyt Quitain </a>
+</br>
+<br>
+          <a href="Student12.php">About to Juan Miguel Roxas</a>
+</br>
+<br>
+          <a href="Student13.php"></a>
+</br>
+<br>
+          <a href="Student14.php"></a>
+</br>
+<br>
+          <a href="Student15.php"></a>
+
+
+      </ul>
+  </body>
+  </html>           
